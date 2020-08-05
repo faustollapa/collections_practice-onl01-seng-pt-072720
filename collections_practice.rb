de2e329df60602c08bc3d7ff07319a578693e425
Fausto Llapa <faustollapa850@gmail.com>
2020-08-05 16:51:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,4 +6,7 @@ def sort_array_desc(array)
   array.sort {|x,y| y <=> x }
 end
   
-def sort_array_char_count(array) 
+def sort_array_char_count(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
